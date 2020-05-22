107a2f731371970e335a9d96b05fb6cf89f55341
luod <3472932635@qq.com>
2020-05-22 17:40:59 +0800

    增加了基础类

--- app/src/main/java/com/anlu/demondk/LivePusher.java
+++ app/src/main/java/com/anlu/demondk/LivePusher.java
@@ -6,28 +6,48 @@ import android.view.SurfaceHolder;
 import com.anlu.demondk.meida.AudioChannel;
 import com.anlu.demondk.meida.VideoChannel;
 
-public class LivePusher {
 
+public class LivePusher {
     private AudioChannel audioChannel;
     private VideoChannel videoChannel;
-
     static {
         System.loadLibrary("native-lib");
     }
 
-    public LivePusher(Activity activity, int width, int height, int bitrate, int fps, int cameraId) {
-
+    public LivePusher(Activity activity, int width, int height, int bitrate,
+                      int fps, int cameraId) {
+        //native_init();
         videoChannel = new VideoChannel(this, activity, width, height, bitrate, fps, cameraId);
-       // audioChannel = new AudioChannel(this);
-    }
+        audioChannel = new AudioChannel(this);
 
+
+    }
     public void setPreviewDisplay(SurfaceHolder surfaceHolder) {
         videoChannel.setPreviewDisplay(surfaceHolder);
     }
-
     public void switchCamera() {
         videoChannel.switchCamera();
-
     }
 
+    public void startLive(String path) {
+       // native_start(path);
+        videoChannel.startLive();
+        audioChannel.startLive();
+    }
+  /*  // 初始化x264
+    public native void native_init();
+    public native void native_setVideoEncInfo(int width, int height, int fps, int bitrate);
+    // 开始推流，传url，子线程运行到阻塞队列，等待传入推流数据
+    public native void native_start(String path);
+    // 传入推流数据
+    public native void native_pushVideo(byte[] data);
+
+    // 推入音频流
+    public native void native_pushAudio(byte[] bytes);
+
+    public native int getInputSamples();
+
+    public native void native_setAudioEncInfo(int i, int channels);
+
+    public native void native_release();*/
 }
--- app/src/main/java/com/anlu/demondk/meida/VideoChannel.java
+++ app/src/main/java/com/anlu/demondk/meida/VideoChannel.java
@@ -1,35 +1,54 @@
 package com.anlu.demondk.meida;
 
 import android.app.Activity;
+import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 import com.anlu.demondk.LivePusher;
 
-public class VideoChannel {
+
+public class VideoChannel implements Camera.PreviewCallback, CameraHelper.OnChangedSizeListener {
+    private static final String TAG = "tuch";
     private CameraHelper cameraHelper;
-    private int mBitrate;//帧率
-    private int mFps;//刷新率
+    private int mBitrate;
+    private int mFps;
     private boolean isLiving;
+    LivePusher livePusher;
 
     public VideoChannel(LivePusher livePusher, Activity activity, int width, int height, int bitrate, int fps, int cameraId) {
-
-        this.mBitrate=bitrate;
-        this.mFps=fps;
-        cameraHelper=new CameraHelper(activity,cameraId,width,height);
+        mBitrate = bitrate;
+        mFps = fps;
+        this.livePusher = livePusher;
+        cameraHelper = new CameraHelper(activity, cameraId, width, height);
         cameraHelper.setPreviewCallback(this);
         cameraHelper.setOnChangedSizeListener(this);
     }
 
-
-
-    public void setPreviewDisplay(SurfaceHolder surfaceHolder) {
-
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        Log.i(TAG, "获取到一帧   onPreviewFrame: ");
+        // 摄像头打开了。会一直调用，所以需要标志位控制
+        // data数据就是一帧的数据，格式是nv21
+      /*  if (isLiving) {
+            livePusher.native_pushVideo(data);
+        }*/
     }
 
+    @Override
+    public void onChanged(int w, int h) {
+       // livePusher.native_setVideoEncInfo(w, h, mFps, mBitrate);
+    }
 
     public void switchCamera() {
-
-
         cameraHelper.switchCamera();
+    }
+
+    public void setPreviewDisplay(SurfaceHolder surfaceHolder) {
+        cameraHelper.setPreviewDisplay(surfaceHolder);
+    }
+
+    public void startLive() {
+        isLiving = true;
     }
 }
